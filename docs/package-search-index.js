31356c5dd5d4e1e9fe5589eaeedc6190e8fad66a
Youtube-Lede <68679956+Youtube-Lede@users.noreply.github.com>
2021-10-01 09:42:32 +0900

    Add javadoc

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.mingcraft.minglib"},{"l":"com.mingcraft.minglib.colors"},{"l":"com.mingcraft.minglib.events.db"},{"l":"com.mingcraft.minglib.exceptions.db"},{"l":"com.mingcraft.minglib.exceptions.log"},{"l":"com.mingcraft.minglib.logs"}];updateSearchResults();
