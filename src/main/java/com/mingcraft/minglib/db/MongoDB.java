cd032ef3590a0fe6112b773ddee55d567dc634ee
Youtube-Lede <68679956+Youtube-Lede@users.noreply.github.com>
2021-10-02 02:08:34 +0900

    Add MongoDB events

--- src/main/java/com/mingcraft/minglib/db/MongoDB.java
+++ src/main/java/com/mingcraft/minglib/db/MongoDB.java
@@ -1,8 +1,11 @@
 package com.mingcraft.minglib.db;
 
+import com.google.api.client.util.Lists;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.mingcraft.minglib.MingLib;
+import com.mingcraft.minglib.events.db.MongoDownloadFinishEvent;
+import com.mingcraft.minglib.events.db.MongoUploadFinishEvent;
 import com.mingcraft.minglib.exceptions.db.MongoConnectionFailedException;
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
@@ -10,11 +13,18 @@ import com.mongodb.ServerAddress;
 import com.mongodb.client.*;
 import com.mongodb.client.model.Filters;
 import org.bson.Document;
+import org.bukkit.Bukkit;
+import org.jetbrains.annotations.Nullable;
 
+import java.io.*;
 import java.util.*;
 
+/**
+ * MingCraft MongoDB API
+ */
 public class MongoDB {
 
+    private static final String KEY_PLAYER = "uuid";
     private static final Gson gson = new GsonBuilder().create();
 
     private static final Map<String, MongoDB> COLLECTION_MAP = new HashMap<>();
@@ -29,6 +39,11 @@ public class MongoDB {
         setupCollection();
     }
 
+    /**
+     * Get MongoDB with key
+     * @param key MongoCollection key
+     * @return MongoDB
+     */
     public static MongoDB getMongoDB(String key) {
         if (!hasConnection(key)) {
             registerCollection(key);
@@ -36,78 +51,169 @@ public class MongoDB {
         return COLLECTION_MAP.get(key);
     }
 
+    /**
+     * Remove the collection with the key as the name from the connected collection list.
+     * @param key MongoCollection key
+     */
+    public static void disconnect(String key) {
+        if (hasConnection(key)) {
+            getMongoDB(key).disconnect();
+        }
+        else {
+            throw new MongoConnectionFailedException("Could not find the collection with key.");
+        }
+    }
+
+    /**
+     * Get MongoClient
+     * @return MongoClient
+     */
     public MongoClient getClient() {
         return client;
     }
 
+    /**
+     * Get MongoDatabase
+     * @return MongoDatabase
+     */
     public MongoDatabase getDatabase() {
         return database;
     }
 
+    /**
+     * Get MongoCollection
+     * @return MongoCollection
+     */
     public MongoCollection<Document> getCollection() {
         return collection;
     }
 
+    /**
+     * Get key
+     * @return Key, MongoCollection name
+     */
     public String getKey() {
         return key;
     }
 
+    /**
+     * Update one document by inquiring MongoCollection with key and value.
+     * @param document Document that you want to update.
+     * @param key The key inside the document to be updated.
+     * @param value The value that matches the key.
+     */
     public void updateOne(Document document, String key, Object value) {
         deleteOne(key, value);
         insertOne(document);
+        callMongoUploadFinishEvent(key, collection, new ArrayList<>() {{add(document);}}, getPlayerKey(document));
     }
 
+    /**
+     * Update many documents by inquiring MongoCollection with key and value.
+     * @param documents Documents that you want to update.
+     * @param key The key inside the document to be updated.
+     * @param value The value that matches the key.
+     */
     public void updateMany(List<Document> documents, String key, Object value) {
         deleteMany(key, value);
         insertMany(documents);
+        callMongoUploadFinishEvent(key, collection, documents, getPlayerKey(documents.get(0)));
     }
 
+    /**
+     * Update many documents by inquiring MongoCollection with key.
+     * @param documents Documents that you want to update.
+     * @param key The key inside the document to be updated.
+     */
     public void updateMany(List<Document> documents, String key) {
         deleteMany(key);
         insertMany(documents);
+        callMongoUploadFinishEvent(key, collection, documents, getPlayerKey(documents.get(0)));
     }
 
+    /**
+     * Update all documents in MongoCollection.
+     * @param documents Documents that you want to update.
+     */
     public void updateAll(List<Document> documents) {
         deleteAll();
         insertMany(documents);
+        callMongoUploadFinishEvent(key, collection, documents, getPlayerKey(documents.get(0)));
     }
 
+    /**
+     * Insert one document to MongoCollection
+     * @param document Document that you want to insert
+     */
     public void insertOne(Document document) {
         collection.insertOne(document);
+        callMongoUploadFinishEvent(key, collection, new ArrayList<>() {{add(document);}}, getPlayerKey(document));
     }
 
+    /**
+     * Insert many documents to MongoCollection
+     * @param documents Documents that you want to insert
+     */
     public void insertMany(List<Document> documents) {
         collection.insertMany(documents);
+        callMongoUploadFinishEvent(key, collection, documents, getPlayerKey(documents.get(0)));
     }
 
+    /**
+     * Download one document from MongoCollection with key and value.
+     * @param key The key inside the document to download.
+     * @param value The value that matches the key.
+     * @return Downloaded document.
+     */
     public Document downloadOne(String key, Object value) {
-        return collection.find(Filters.eq(key, value)).first();
+        Document document = collection.find(Filters.eq(key, value)).first();
+        assert document != null;
+        callMongoDownloadFinishEvent(this.key, collection, new ArrayList<>() {{add(document);}}, getPlayerKey(document));
+        return document;
     }
 
-    public Set<Document> downloadMany(String key, Object value) {
+    /**
+     * Download many documents from MongoCollection with key and value.
+     * @param key The key inside the document to download.
+     * @param value The value that matches the key.
+     * @return Downloaded documents.
+     */
+    public List<Document> downloadMany(String key, Object value) {
         MongoCursor<Document> cursor = collection.find(Filters.eq(key, value)).iterator();
-        Set<Document> documents = new HashSet<>();
+        List<Document> documents = new ArrayList<>();
         while (cursor.hasNext()) {
             documents.add(cursor.next());
         }
+        callMongoDownloadFinishEvent(this.key, collection, documents, getPlayerKey(documents.get(0)));
         return documents;
     }
 
-    public Set<Document> downloadMany(String key) {
+    /**
+     * Download many documents from MongoCollection with key.
+     * @param key The key inside the document to download.
+     * @return Downloaded documents.
+     */
+    public List<Document> downloadMany(String key) {
         MongoCursor<Document> cursor = collection.find(Filters.exists(key)).iterator();
-        Set<Document> documents = new HashSet<>();
+        List<Document> documents = new ArrayList<>();
         while (cursor.hasNext()) {
             documents.add(cursor.next());
         }
+        callMongoDownloadFinishEvent(this.key, collection, documents, getPlayerKey(documents.get(0)));
         return documents;
     }
 
-    public Set<Document> downloadAll() {
+    /**
+     * Download all documents from MongoCollection.
+     * @return Downloaded documents.
+     */
+    public List<Document> downloadAll() {
         MongoCursor<Document> cursor = collection.find().iterator();
-        Set<Document> documents = new HashSet<>();
+        List<Document> documents = new ArrayList<>();
         while (cursor.hasNext()) {
             documents.add(cursor.next());
         }
+        callMongoDownloadFinishEvent(this.key, collection, documents, getPlayerKey(documents.get(0)));
         return documents;
     }
 
@@ -125,6 +231,11 @@ public class MongoDB {
 
     public void deleteAll() {
         collection.drop();
+    }
+
+    public void uploadFile(File file) throws FileNotFoundException {
+        InputStream inputStream = new FileInputStream(file);
+
     }
 
     public static String toJson(Document document) {
@@ -149,6 +260,10 @@ public class MongoDB {
 
     public static Object toObject(Document document, Class<?> clazz) {
         return gson.fromJson(document.toJson(), clazz);
+    }
+
+    public static Set<String> getKeySet() {
+        return COLLECTION_MAP.keySet();
     }
 
     public static void registerMongoDB() {
@@ -212,7 +327,29 @@ public class MongoDB {
         collection = database.getCollection(key);
     }
 
-    private static boolean hasConnection(String key) {
+    private void disconnect() {
+        COLLECTION_MAP.remove(key);
+    }
+
+    public static boolean hasConnection(String key) {
         return COLLECTION_MAP.containsKey(key);
+    }
+
+    private static boolean hasPlayerKey(Document document) {
+        return document.containsKey(KEY_PLAYER);
+    }
+
+    private static UUID getPlayerKey(Document document) {
+        return UUID.fromString((String) document.get(KEY_PLAYER));
+    }
+
+    private static void callMongoUploadFinishEvent(String key, MongoCollection<Document> collection, List<Document> documents, @Nullable UUID player) {
+        MongoUploadFinishEvent event = new MongoUploadFinishEvent(key, collection, documents, player);
+        Bukkit.getPluginManager().callEvent(event);
+    }
+
+    private static void callMongoDownloadFinishEvent(String key, MongoCollection<Document> collection, List<Document> documents, @Nullable UUID player) {
+        MongoDownloadFinishEvent event = new MongoDownloadFinishEvent(key, collection, documents, player);
+        Bukkit.getPluginManager().callEvent(event);
     }
 }
---
+++ src/main/java/com/mingcraft/minglib/events/db/MongoUploadFinishEvent.java
@@ -0,0 +1,64 @@
+package com.mingcraft.minglib.events.db;
+
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.UUID;
+
+public class MongoUploadFinishEvent extends Event {
+
+    private static final HandlerList handlers = new HandlerList();
+
+    private final String key;
+    private final MongoCollection<Document> collection;
+    private final List<Document> documents;
+    private final UUID player;
+
+    public MongoUploadFinishEvent(String key, MongoCollection<Document> collection, List<Document> documents) {
+        this.key = key;
+        this.collection = collection;
+        this.documents = documents;
+        this.player = null;
+    }
+
+    public MongoUploadFinishEvent(String key, MongoCollection<Document> collection, List<Document> documents, UUID player) {
+        this.key = key;
+        this.collection = collection;
+        this.documents = documents;
+        this.player = player;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public MongoCollection<Document> getCollection() {
+        return collection;
+    }
+
+    public List<Document> getDocuments() {
+        return documents;
+    }
+
+    public UUID getPlayer() {
+        return player;
+    }
+
+    public boolean isPlayerDocument() {
+        return player != null;
+    }
+
+    @Override
+    public @NotNull HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+
+}
