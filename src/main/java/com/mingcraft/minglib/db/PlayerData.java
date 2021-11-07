c3fb538d8bd636da16cd36b0a265e427a166629c
Youtube-Lede <68679956+Youtube-Lede@users.noreply.github.com>
2021-11-07 18:41:57 +0900

    make public

--- src/main/java/com/mingcraft/minglib/db/PlayerData.java
+++ src/main/java/com/mingcraft/minglib/db/PlayerData.java
@@ -2,10 +2,10 @@ package com.mingcraft.minglib.db;
 
 public abstract class PlayerData {
 
-    abstract Object getPlayerData(String uuid);
+    public abstract Object getPlayerData(String uuid);
 
-    abstract boolean hasPlayerData(String uuid);
+    public abstract boolean hasPlayerData(String uuid);
 
-    abstract void initPlayerData(String uuid);
+    public abstract void initPlayerData(String uuid);
 
 }
