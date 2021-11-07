d4c6b55316af7a8eb961179187fa1399dcc59dc3
Youtube-Lede <68679956+Youtube-Lede@users.noreply.github.com>
2021-11-07 19:04:31 +0900

    remove methods

--- src/main/java/com/mingcraft/minglib/db/PlayerData.java
+++ src/main/java/com/mingcraft/minglib/db/PlayerData.java
@@ -2,10 +2,4 @@ package com.mingcraft.minglib.db;
 
 public interface PlayerData {
 
-    Object getPlayerData(String uuid);
-
-    boolean hasPlayerData(String uuid);
-
-    void initPlayerData(String uuid);
-
 }
